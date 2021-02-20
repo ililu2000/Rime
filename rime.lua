0770347cb708db534c79b587b4f0defb0cca357c
ililu2000 <78154435+ililu2000@users.noreply.github.com>
2021-02-20 10:03:29 +0800

    增加【插入当前日期时间】的功能

---
+++ rime.lua
@@ -0,0 +1,17 @@
+function date_translator(input, seg)
+    if (input == "rq") then
+        --- Candidate(type, start, end, text, comment)
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), ""))
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), ""))
+    end
+    if (input == "sj") then
+        --- Candidate(type, start, end, text, comment)
+        yield(Candidate("time", seg.start, seg._end, os.date("%Y年%m月%d日 %H:%M:%S"), ""))
+        yield(Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), ""))
+    end
+    if (input == "xq") then
+        local weakTab = {'日', '一', '二', '三', '四', '五', '六'}
+        yield(Candidate("week", seg.start, seg._end, "星期"..weakTab[tonumber(os.date("%w")+1)], ""))
+        yield(Candidate("week", seg.start, seg._end, "周"..weakTab[tonumber(os.date("%w")+1)], ""))
+    end
+end
